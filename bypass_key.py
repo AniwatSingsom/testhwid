a85230c9d0abb4f29ecc3e596938aabfe68e0012
AniwatSingsom <110642544+AniwatSingsom@users.noreply.github.com>
2024-02-03 00:30:49 +0700

    Update bypass_key.py

--- bypass_key.py
+++ bypass_key.py
@@ -1,34 +1,36 @@
-from appium import webdriver
-from appium.webdriver.common.touch_action import TouchAction
-from appium.webdriver.common.multi_action import MultiAction
+import sys
+import time
+import pyperclip
+from seleniumwire import webdriver
+from seleniumwire.utils import decode
+from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style, init
+from fake_useragent import UserAgent
+from seleniumwire import webdriver
+from seleniumwire.utils import decode
+from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style, init
+from fake_useragent import UserAgent
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from fake_useragent import UserAgent
-import time
-import pyperclip
-from colorama import Fore, Back, Style, init
 
 init()
-
 def getkey(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     code_elements = soup.find_all('code', class_='aos-init aos-animate')
     if code_elements:
         return code_elements[0].text.strip()
     return None
-
 def readhwid(filename):
     try:
         with open(filename, 'r') as file:
             return file.read().strip()
     except FileNotFoundError:
         return None
-
 def writehwid(filename, hwid):
     with open(filename, 'w') as file:
         file.write(hwid)
-
 def gethwid():
     filename = "hwid"
 
@@ -54,26 +56,23 @@ def gethwid():
         writehwid(filename, hwid)
         print(Fore.GREEN + "Successful")
         return hwid
-
 def main():
     hwid = gethwid()
     base_url = f"https://keysystem.fluxteam.net/android/checkpoint/start.php?HWID={hwid}"
     UA = UserAgent()
     agent = UA.random
-    desired_caps = {
-        'platformName': 'Android',
-        'deviceName': 'Android',
-        'app': '/path/to/appium-apk-file.apk',
-        'appPackage': 'com.fluxteam.keysystem',
-        'appActivity': '.MainActivity',
-        'noReset': 'true',
-        'userAgent': agent
-    }
-    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument(f"user-agent={agent}")
+    options.add_argument("--window-size=500,500")
+    options.add_argument("--log-level=3")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
     try:
         driver.get(base_url)
         driver.header_overrides = {'Referer': base_url}
         driver.get(f"{base_url}&7b20bcc1dfe26db966bb84f159da392f=false")
+        driver.header_overrides = {'Referer': 'https://linkvertise.com'}
         driver.get("https://fluxteam.net/android/checkpoint/check1.php")
         driver.get("https://fluxteam.net/android/checkpoint/main.php")
         wait = WebDriverWait(driver, 10)
